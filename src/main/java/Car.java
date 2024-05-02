76ae9cf2a91db439d2b21bd45134dfc7f4d0e670
Han <hkjbrian@naver.com>
2024-05-02 16:07:05 +0900

    Step1

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,16 +1,16 @@
 import java.util.Random;
 
 public class Car {
-
+    final private static int DEFAULT_POSITION = 0
     private String carName;
-    private int carPosiiton;
+    private int carPosition = DEFAULT_POSITION;
 
     public void randomCarMoving() {
         Random random = new Random();
         int randomNumber = random.nextInt(0, 10);
 
         if (randomNumber >= 4) {
-            carPosiiton++;
+            carPosition++;
         }
     }
 }
